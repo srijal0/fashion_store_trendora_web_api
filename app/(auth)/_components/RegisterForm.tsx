ca2630aaa9b13f4bd4e309ce85ada96a06a44b7b
Shreejal Shrestha <shresthashreejal84@gmail.com>
2025-12-27 18:42:38 +0545

    completed

--- app/(auth)/_components/RegisterForm.tsx
+++ app/(auth)/_components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+//jhuug
+//mkjhibjbu
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -9,5 +9,3 @@ export default function Page() {
     </div>
   );
 }
-//register page
-//kjjl
