de4dd295d7bc133567500aa852763232b004a944
Shreejal Shrestha <shresthashreejal84@gmail.com>
2026-01-18 21:07:14 +0545

    installed axios packages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             className="object-cover"
           />
         </div>
-//RIGHT
+
         {/* RIGHT SIDE – FORM */}
         <div className="flex h-full items-center justify-center px-4 md:px-10 bg-rose-50">
           {children}
